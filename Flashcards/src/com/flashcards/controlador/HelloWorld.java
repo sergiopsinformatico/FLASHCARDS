efbadfff1828b291d9152f40e3c5fc705f62d3c8
Sergio Perez Sanchez <sergio.perez9@alu.uclm.es>
2018-03-10 14:22:20 +0100

    Controlador de prueba creado

---
+++ Flashcards/src/com/flashcards/controlador/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.flashcards.controlador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorld {
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** FLASHCARDS</h3>Página de Inicio **********</div><br><br>";
+		return new ModelAndView("welcome", "message", message);
+	}
+}
