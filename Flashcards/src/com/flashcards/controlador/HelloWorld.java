06529cd27bb914e23bdb76b84baf8cecaf692eba
Sergio Perez Sanchez <sergio.perez9@alu.uclm.es>
2018-03-15 17:15:56 +0100

    Proyecto base creado y funcionando en local

--- Flashcards/src/com/flashcards/controlador/HelloWorld.java
+++ Flashcards/src/com/flashcards/controlador/HelloWorld.java
@@ -2,6 +2,7 @@ package com.flashcards.controlador;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -9,7 +10,12 @@ public class HelloWorld {
 	@RequestMapping("/welcome")
 	public ModelAndView helloWorld() {
 		String message = "<br><div style='text-align:center;'>"
-				+ "<h3>********** FLASHCARDS</h3>Página de Inicio **********</div><br><br>";
+				+ "<h3>********** FLASHCARDS</h3>Página de Inicio. Prueba Superada **********</div><br><br>";
 		return new ModelAndView("welcome", "message", message);
 	}
 }
+
+
+//@RequestMapping("/welcome.htm")
+//@RequestMapping(value = "welcome", method = RequestMethod.GET)
+//HttpServletRequest request, HttpServletResponse response, Model model
