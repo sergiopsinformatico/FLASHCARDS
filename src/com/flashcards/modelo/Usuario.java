64757bf08c53e1eed1cc9021f0e08e5fad921059
Sergio Perez Sanchez <sergiopsinformatico@gmail.com>
2018-03-20 18:54:12 +0100

    Clase Usuario

---
+++ src/com/flashcards/modelo/Usuario.java
@@ -0,0 +1,81 @@
+package com.flashcards.modelo;
+
+public class Usuario {
+	String usuario, clave, email, nombre, apellidos, ciudad, pais;
+	int edad;
+	
+	public Usuario(String usuario, String clave, String email, String nombre, String apellidos, int edad, String ciudad, String pais) {
+		setUsuario(usuario);
+		setClave(clave);
+		setEmail(email);
+		setNombre(nombre);
+		setApellidos(apellidos);
+		setEdad(edad);
+		setCiudad(ciudad);
+		setPais(pais);
+	}
+
+	public String getUsuario() {
+		return this.usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getClave() {
+		return this.clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return this.apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public int getEdad() {
+		return this.edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getCiudad() {
+		return this.ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public String getPais() {
+		return this.pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+}
