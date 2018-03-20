8bf81de410f37932c3eed0a1b42c2479033e2544
Sergio Perez Sanchez <sergiopsinformatico@gmail.com>
2018-03-20 18:35:00 +0100

    Creacion del esqueleto de las pruebas

---
+++ src/tests/java/CrearUsuarioTest.java
@@ -0,0 +1,95 @@
+package tests.java;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CrearUsuarioTest {
+	
+	//Scenario 1: Registrar un usuario exitosamente
+	
+	@Given("^Una persona quiere registrarse$")
+	public void una_persona_quiere_registrarse() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Introduce los datos$")
+	public void introduce_los_datos() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Se registra correctamente$")
+	public void se_registra_correctamente() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	
+	//Scenario 2: Existe email
+	
+	@Given("^Una persona desea registrarse$")
+	public void una_persona_desea_registrarse() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Introduce un email existente$")
+	public void introduce_un_email_existente() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^No puede registrarse$")
+	public void no_puede_registrarse() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	
+	//Scenario 3: Existe nombre de usuario
+	
+	@Given("^Una persona va a registrarse$")
+	public void una_persona_va_a_registrarse() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Introduce un nombre de usuario existente$")
+	public void introduce_un_nombre_de_usuario_existente() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^La cuenta no se crea$")
+	public void la_cuenta_no_se_crea() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	
+	//Scenario 4: La password no cumple con los requisitos
+	
+	@Given("^Un usuario quiere registrarse$")
+	public void un_usuario_quiere_registrarse() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^La clave no cumple los requisitos$")
+	public void la_clave_no_cumple_los_requisitos() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^No se crea la cuenta$")
+	public void no_se_crea_la_cuenta() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+
+	
+}
