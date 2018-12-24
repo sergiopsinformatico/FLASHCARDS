1e82e445a720ee2c6212c791a3fe61859e249ac5
Sergio Perez Sanchez <sergiopsinformatico@gmail.com>
2018-12-24 15:10:13 +0100

    Crear usuario

--- src/main/java/flashcards/controladores/ControladorRegistro.java
+++ src/main/java/flashcards/controladores/ControladorRegistro.java
@@ -40,8 +40,7 @@ public class ControladorRegistro {
 		return vista;
 	}
 	
-	/*@RequestMapping(value = "registrarUser", method = RequestMethod.POST, consumes = {"application/json;charset=UTF-8"}, produces={"application/json;charset=UTF-8"})*/
-	@PostMapping(value = "/registrarUser")
+	@RequestMapping(value = "/createUser", method = RequestMethod.POST, consumes = {"application/json;charset=UTF-8"}, produces={"application/json;charset=UTF-8"})
 	public ResponseEntity<String> registrarUsuarioPost(@RequestBody UsuarioDTO user) {
 		broker = new Broker();
 		dBUsuario = broker.getInstanciaUsuario();
