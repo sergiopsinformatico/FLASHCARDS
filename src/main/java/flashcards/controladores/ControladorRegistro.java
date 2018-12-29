a1a69b6687ced35acb87778f0559dc27a35b4782
Sergio Perez Sanchez <sergiopsinformatico@gmail.com>
2018-12-29 18:09:35 +0100

    Registro de usuario

--- src/main/java/flashcards/controladores/ControladorRegistro.java
+++ src/main/java/flashcards/controladores/ControladorRegistro.java
@@ -34,8 +34,8 @@ public class ControladorRegistro {
 	}
 	
 	@RequestMapping(value = "/guardarUsuario", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody ResponseEntity<Void> registrarUsuarioPost(@Valid UsuarioDTO user) {
-		return new ResponseEntity<Void>(HttpStatus.OK);
+	public @ResponseBody ResponseEntity<String> registrarUsuarioPost(@Valid UsuarioDTO user) {
+		return new ResponseEntity<String>("Llega correctamente", HttpStatus.OK);
 		
 	}
 	
