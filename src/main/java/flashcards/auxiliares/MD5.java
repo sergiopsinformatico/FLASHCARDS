4a615108613caa5ca4e9525bdb029f025e19e88d
Sergio Perez Sanchez <sergiopsinformatico@gmail.com>
2019-01-25 21:23:31 +0100

    Mejoras aplicadas a las sugerencias de SonarCloud

--- src/main/java/flashcards/auxiliares/MD5.java
+++ src/main/java/flashcards/auxiliares/MD5.java
@@ -1,13 +1,15 @@
 package main.java.flashcards.auxiliares;
 import java.security.MessageDigest;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 public class MD5 {
 	private MD5() {
 		throw new IllegalStateException("Genera Codigo MD5 para Gravatar");
 	}
 	
 	public static String hex(byte[] array) {
-		StringBuffer sb = new StringBuffer();
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < array.length; ++i) {
 			sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
 		}
@@ -15,11 +17,13 @@ public class MD5 {
 	}
 	
 	public static String getMD5Encryption (String message, String cryptographicAlgorithm, String codingBytes) {
-		try {
+		/*try {
 			MessageDigest md = MessageDigest.getInstance(cryptographicAlgorithm);
+			
 			return hex(md.digest(message.getBytes(codingBytes)));
 		} catch (Exception e) {
 			return "";
-		}
+		}*/
+		return DigestUtils.md5Hex(message);
 	}
 }
