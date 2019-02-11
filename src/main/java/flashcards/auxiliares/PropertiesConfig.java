3abe563c12fc5923dc445e3926538e5eaf10cf7f
Sergio Perez Sanchez <sergiopsinformatico@gmail.com>
2019-02-11 23:00:53 +0100

    Optimizacion SonarCloud Sprint 1

--- src/main/java/flashcards/auxiliares/PropertiesConfig.java
+++ src/main/java/flashcards/auxiliares/PropertiesConfig.java
@@ -1,18 +1,12 @@
 package main.java.flashcards.auxiliares;
 
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
-
-import org.springframework.beans.factory.annotation.Value;
 
 public class PropertiesConfig {
 	
 	private PropertiesConfig() {}
-	
-	@Value("file")
-	final private static String filePath="filePath";
-	
+		
 	public static String getProperties(String name) {
 		return getValue(name);
 	}
@@ -21,7 +15,7 @@ public class PropertiesConfig {
 		String property;
 		try {
 			Properties prop = new Properties();
-		    InputStream inputStream = new FileInputStream(filePath);
+		    InputStream inputStream = PropertiesConfig.class.getClassLoader().getResourceAsStream("conexionDB.properties");
 		    prop.load(inputStream);
 		    property =  prop.getProperty(name);
 		    inputStream.close();
