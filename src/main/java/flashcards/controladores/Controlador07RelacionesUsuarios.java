8d7bf2cb918f18014d835666a363b1ab66230a55
Sergio Perez Sanchez <sergiopsinformatico@gmail.com>
2019-03-27 23:58:57 +0100

    Vista Personas

--- src/main/java/flashcards/controladores/Controlador07RelacionesUsuarios.java
+++ src/main/java/flashcards/controladores/Controlador07RelacionesUsuarios.java
@@ -1,5 +1,6 @@
 package main.java.flashcards.controladores;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -34,6 +35,12 @@ public class Controlador07RelacionesUsuarios {
 	String json;
 	RelacionDTO relacion;
 	ArrayList<String> lista;
+	List<UsuarioDTO> listaBas;
+	List<UsuarioDTO> listaDef;
+	UsuarioDTO user;
+	int indice;
+	int indiceDos;
+	boolean encontrado;
 	
 	@RequestMapping(value = "/personas", method = RequestMethod.GET)
 	public ModelAndView personas(HttpServletRequest request, HttpServletResponse response) {
@@ -48,9 +55,24 @@ public class Controlador07RelacionesUsuarios {
 	public List<UsuarioDTO> allPeople(@RequestParam("username") String username) {
 		dBUsuario = Broker.getInstanciaUsuario();
 		dBRelacion = Broker.getInstanciaRelacion();
+		
+		listaBas = dBUsuario.getAllUsersSystem(username);
+		
 		relacion = dBRelacion.leerRelacionUsuario(username);
-		relacion.getBloqueadoPor();
-		return dBUsuario.getAllUsersSystem(username);
+		lista = relacion.getBloqueadoPor();
+		
+		listaDef = new LinkedList<UsuarioDTO>();
+		
+		for(indice=0; indice<listaBas.size(); indice++) {
+			user = listaBas.get(indice);
+			encontrado = false;
+			for(indiceDos=0; indiceDos<lista.size(); indiceDos++) {
+				if(lista.get(indiceDos).equals(user.getUsername())) {
+					encontrado = true;
+				}
+			}
+		}
+		return listaDef;
 	}
 	
 	@RequestMapping(value = "/verUsuario", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -58,13 +80,63 @@ public class Controlador07RelacionesUsuarios {
 	@ResponseStatus(HttpStatus.OK)
 	public ModelAndView verUsuario(@RequestParam("username") String username) {
 		dBRelacion = Broker.getInstanciaRelacion();
+		dBUsuario = Broker.getInstanciaUsuario();
+		
 		relacion = dBRelacion.leerRelacionUsuario(username);
 		
 		vista = new ModelAndView("vistaPerfil");
-		relacion.getAmigos();
-		relacion.getBloqueados();
-		relacion.getPeticionesEnviadas();
-		relacion.getPeticionesRecibidas();
+		vista.addObject("perfil", dBUsuario.getUsuarioDTO(username));
+		
+		encontrado = false;
+		
+		lista = relacion.getAmigos();
+		for(indice=0; indice<lista.size(); indice++) {
+			if(lista.get(indice).equals(username)) {
+				encontrado = true;
+				vista.addObject("relacion", "amigos");
+				break;
+			}
+		}
+		
+		if(!encontrado) {
+			lista = relacion.getBloqueados();
+			for(indice=0; indice<lista.size(); indice++) {
+				if(lista.get(indice).equals(username)) {
+					encontrado = true;
+					vista.addObject("relacion", "bloqueado");
+					break;
+				}
+			}
+			if(!encontrado) {
+				
+				lista = relacion.getPeticionesEnviadas();
+				for(indice=0; indice<lista.size(); indice++) {
+					if(lista.get(indice).equals(username)) {
+						encontrado = true;
+						vista.addObject("relacion", "pdaEnvia");
+						break;
+					}
+				}
+				
+				if(!encontrado) {
+					lista = relacion.getPeticionesRecibidas();
+					for(indice=0; indice<lista.size(); indice++) {
+						if(lista.get(indice).equals(username)) {
+							encontrado = true;
+							vista.addObject("relacion", "pdaRecibe");
+							break;
+						}
+					}
+					
+					if(!encontrado) {
+						vista.addObject("relacion", "none");
+					}
+				}
+			}
+		}
+		
+		
+		
 		return vista;
 	}
 	
